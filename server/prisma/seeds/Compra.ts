345c8618d340b3a1a46f8d5058903cdc6bb027c8
gabulate <gabrielulate71@gmail.com>
2023-06-14 15:42:45 -0600

    Avance 2.2.2

--- server/prisma/seeds/Compra.ts
+++ server/prisma/seeds/Compra.ts
@@ -4,7 +4,6 @@ export const Compras = [
       ClienteId: 1,
       DireccionId: 1,
       MetodoPagoId: 1,
-      EstadoCompraId: 1,
       Subtotal: "30000",
       Total: "33900",
     },
--- server/prisma/seeds/CompraDetalle.ts
+++ server/prisma/seeds/CompraDetalle.ts
@@ -4,6 +4,7 @@ export const CompraDetalles = [
       CompraId: 1,
       ProductoId: 1,
       Cantidad: 1,
+      EstadoCompraId: 1,
       Subtotal: "30000",
     },
   ];
